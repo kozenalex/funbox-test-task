c92a13810f2ee6dc44bb6ca873fd25148ef93285
Aleksey Kosin <akosin@udm.ru>
2023-02-03 17:07:08 +0400

    some starting tests with fastapi and redis

---
+++ my_api.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import redis
+from urllib.parse import urlparse
+import datetime, time
+from json import loads, dumps
+
+app = FastAPI()
+
+class RequestModel(BaseModel):
+
+    links: list
+
+r = redis.StrictRedis(
+    host='127.0.0.1',
+    port=6379,
+    password=''
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get('/clear_db')
+async def clear_db():
+    keys = r.keys('*')
+    r.delete(*keys)
+    return {
+        'deleted keys': keys
+    }
+@app.get("/visited_links")
+async def show_links():
+    answer = []
+    keys = r.keys('*')
+    for key in keys:
+        answer.extend(r.hmget(key, 'domains')[0].split())
+    return {
+        'domains': set(answer),
+        'status': 'ok'
+    }
+
+@app.post("/visited_links")
+async def get_links(request: Request):
+    now = int(time.mktime(datetime.datetime.now().timetuple()))
+    answer = await request.json()
+    domains = [
+        urlparse(l).netloc if urlparse(l).scheme else urlparse('//'+l).netloc for l in answer["links"]
+     ]
+    domains_set = set(domains)
+    r.hmset(now, {'domains': ' '.join(domains_set)})
+    return domains_set
