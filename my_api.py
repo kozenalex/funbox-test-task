3cb3c54cd0c48a7d0e2c4e1f9c752df2b584167a
Aleksey Kosin <akosin@udm.ru>
2023-02-08 16:24:18 +0400

    RequestValidationError

--- my_api.py
+++ my_api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
-from fastapi.exceptions import ValidationError
+from fastapi.exceptions import RequestValidationError
 from typing import List
 from pydantic import BaseModel, validator
 import redis
@@ -20,17 +20,16 @@ class RequestModel(BaseModel):
     links: List[str]
 
     @validator('links')
-    def check_links(cls, links):
+    def check_strs(cls, links):
         for link in links:
-            if len(link) < 1:
-            # if not urlparse(link).scheme and not urlparse(link).netloc:
-                raise ValidationError
+            if not urlparse(link).scheme and not urlparse("//" + link).netloc:
+                raise RequestValidationError
         return links
 
-@app.exception_handler(ValidationError)
-async def validation_exception_handler(request: Request, exc: ValidationError):
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
     content = {
-            "status": "wrong data"
+            "status": "POST data is invalid"
         }
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -66,16 +65,16 @@ async def show_links(start:int = 0, to:int = 0):
 @app.post("/visited_links")
 async def get_links(links: RequestModel):
     now = int(time.mktime(datetime.datetime.now().timetuple()))
-    status = 'ok'
-    try:
-        domains = [
-            urlparse(l).netloc if urlparse(l).scheme else urlparse('//'+l).netloc for l in links.dict()["links"]
-        ]
-        domains_set = set(domains)
-        r.hmset(now, {'domains': ' '.join(domains_set)})
-    except ValidationError as e:
-        status = e.json()
-    
+    domains = [
+        urlparse(l).netloc if urlparse(l).scheme else urlparse('//'+l).netloc for l in links.dict()["links"]
+    ]
+    domains_set = set(domains)
+    r.hmset(now, {'domains': ' '.join(domains_set)})
+    answer = None
+    if status.HTTP_200_OK:
+        answer = 'ok'
+    else:
+        answer = status
     return {
-        'status': status
+        'status': answer
     }
